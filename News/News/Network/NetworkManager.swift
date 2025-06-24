74894a57f688f73840f83f8e01d457093ccef79e
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2025-06-25 01:11:23 +0300

    Add: generic network manager layer added

--- News/News/Network/NetworkManager.swift
+++ News/News/Network/NetworkManager.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+
+enum HttpMethod: String {
+    case GET
+    case POST
+}
+
+protocol NetworkManagerProtocol {
+    func request<T: Codable>(
+        url: URL,
+        method: HttpMethod,
+        completion: @escaping (Result<T, NetworkError>) -> Void
+    )
+}
+
+final class NetworkManager {
+    // MARK: - Properties
+    private let session: URLSession
+    
+    // MARK: Init
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+}
+
+extension NetworkManager: NetworkManagerProtocol {
+    
+    func request<T>(url: URL, method: HttpMethod, completion: @escaping (Result<T, NetworkError>) -> Void) where T : Decodable, T : Encodable {
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            
+            if let error = error {
+                completion(.failure(.customError(error)))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                print("Data doğru gelmedi error var. Error eklenecek.....")
+                return
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedData))
+            } catch  {
+                completion(.failure(.decodingError))
+            }
+            
+        }
+        task.resume()
+        
+    }
+    
+    
+}
